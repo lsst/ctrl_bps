5d140bef38e8ccb5ca500ec395d0097111791c16
Michelle Gower <mgower@illinois.edu>
2019-04-15 11:48:36 -0500

    Add bunch of template files

---
+++ SConstruct
@@ -0,0 +1,4 @@
+# -*- python -*-
+from lsst.sconsUtils import scripts
+# Python-only package
+scripts.BasicSConstruct("ctrl_bps", disableCc=True)
---
+++ doc/conf.py
@@ -0,0 +1,13 @@
+"""Sphinx configuration file for an LSST stack package.
+
+This configuration only affects single-package Sphinx documentation builds.
+"""
+
+from documenteer.sphinxconfig.stackconf import build_package_configs
+import lsst.ctrl.bps
+
+
+_g = globals()
+_g.update(build_package_configs(
+    project_name='ctrl_bps',
+    version=lsst.ctrl.bps.version.__version__))
