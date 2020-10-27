07c58820f9aeef3ea2a80ecad7716514a0b51a23
Mikolaj Kowalik <mxk@illinois.edu>
2020-10-27 15:24:14 -0400

    Add initial implementation of Click based CLI

---
+++ python/lsst/ctrl/bps/cli/cmd/commands.py
@@ -0,0 +1,74 @@
+# This file is part of ctrl_bps.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import click
+from lsst.daf.butler.cli.opt import log_level_option, long_log_option
+from lsst.daf.butler.cli.utils import cli_handle_exception
+from .. import opt
+from .. import script
+
+
+@click.command()
+@opt.config_file_argument(required=True)
+@log_level_option()
+def transform(*args, **kwargs):
+    """Transform a quantum graph to a workflow graph.
+    """
+    pass
+
+
+@click.command()
+@opt.config_file_argument(required=True)
+@log_level_option()
+def prepare(*args, **kwargs):
+    """Prepare a workflow for submission.
+    """
+    cli_handle_exception(script.prepare, *args, **kwargs)
+
+
+@click.command()
+@opt.config_file_argument(required=True)
+@log_level_option()
+def submit(*args, **kwargs):
+    """Submit a workflow for execution.
+    """
+    config, workflow = cli_handle_exception(script.prepare, *args, **kwargs)
+    cli_handle_exception(script.submit, config=config, workflow=workflow,
+                         **kwargs)
+
+
+@click.command()
+@log_level_option()
+@click.option("--wms", "wms_service",
+              default="lsst.ctrl.bps.wms.htcondor.htcondor_service.HTCondorService",
+              help="Workload Management System service class")
+@click.option("--user",
+              help="Restrict report to specific user.")
+@click.option("--id", "run_id",
+              help="Restrict report to specific WMS run id.")
+@click.option("--hist", "hist_days",
+              default=0.0,
+              help="Search WMS history X days for completed info.")
+@click.option("--pass-thru",
+              help="Pass the given string to the WMS service class")
+def report(*args, **kwargs):
+    """Display execution status for submitted workflows.
+    """
+    cli_handle_exception(script.report, *args, **kwargs)
---
+++ python/lsst/ctrl/bps/cli/script/prepare.py
@@ -0,0 +1,53 @@
+# This file is part of ctrl_bps.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import logging
+from lsst.daf.butler.cli.cliLog import CliLog
+from ...bps_config import BpsConfig
+from ...bpsub_driver import BPS_SEARCH_ORDER, create_submission
+
+
+def _prepare(config_file, log_level, **kwargs):
+    """Create a workflow for a specific workflow management system.
+
+    Parameters
+    ----------
+    config_file : `str`
+        Name of the configuration file.
+    log_level : `str`
+        Per-component logging levels, each item in the list is a tuple
+        (component, level), `component` is a logger name or an empty string
+        or `None` for root logger, `level` is a logging level name, one of
+        CRITICAL, ERROR, WARNING, INFO, DEBUG (case insensitive).
+
+    Returns
+    -------
+    config : `lsst.ctrl.bps.BpsConfig`
+        Configuration to use when creating the workflow.
+    workflow : `lsst.ctrl.bps.wms_workflow.BaseWmsWorkflow`
+        Representation of the abstract/scientific workflow specific to a given
+        workflow management system.
+    """
+    if log_level is not None:
+        CliLog.setLogLevels(log_level)
+    config = BpsConfig(config_file, BPS_SEARCH_ORDER)
+    workflow = create_submission(config)
+    print(f"Submit dir: {workflow.submit_path}")
+    return config, workflow
