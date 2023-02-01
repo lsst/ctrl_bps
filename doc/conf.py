d5c75fdfdb3be3e6d75fbc5c86a32c52a00cb00e
Tim Jenness <tjenness@lsst.org>
2023-02-01 15:48:40 -0700

    Changes from black 23.1

--- doc/conf.py
+++ doc/conf.py
@@ -6,7 +6,7 @@ This configuration only affects single-package Sphinx documentation builds.
 from documenteer.conf.pipelinespkg import *  # noqa: F403, import *
 
 project = "ctrl_bps"
-html_theme_options["logotext"] = project     # noqa: F405, unknown name
+html_theme_options["logotext"] = project  # noqa: F405, unknown name
 html_title = project
 html_short_title = project
 doxylink = {}
