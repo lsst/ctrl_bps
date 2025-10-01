fc312025e695fe9d4d753992f7ff9115828d2974
Jim Bosch <jbosch@astro.princeton.edu>
2025-10-01 18:25:03 -0400

    Switch to daily docs for intersphinx links in the doc build.

--- doc/conf.py
+++ doc/conf.py
@@ -13,5 +13,5 @@ doxylink = {}
 exclude_patterns = ["changes/*"]
 
 # Try to pull in links for butler and pipe_base.
-intersphinx_mapping["lsst"] = ("https://pipelines.lsst.io/v/weekly/", None)  # noqa
+intersphinx_mapping["lsst"] = ("https://pipelines.lsst.io/v/daily/", None)  # noqa
 intersphinx_mapping["networkx"] = ("https://networkx.org/documentation/stable/", None)  # noqa: F405
